1d1ce5a2525c3f1fa7781411809d7eccb1f3a257
ojasvishaklya <ojasvishaklya@gmail.com>
2021-05-23 01:03:00 +0530

    cors error

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')(process.env.PORT || 3000)
+const io = require('socket.io')(process.env.PORT || 3000, {
+    cors: {
+        origin: '*',
+    }
+})
 const { FPS } = require('./constants');
 const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
 
